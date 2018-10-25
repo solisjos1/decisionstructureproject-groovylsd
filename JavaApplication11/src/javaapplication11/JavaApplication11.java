569f8e8320f64c9c9c9e5ea8611f109a14df871b
guerrjos <guerrjos@011-4303-S12QD.students.cisd.conroeisd.loc>
2018-10-25 08:05:14 -0500

    Decision Structure Project

---
+++ JavaApplication11/src/javaapplication11/JavaApplication11.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication11;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author guerrjos
+ */
+public class JavaApplication11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner kb = new Scanner (System.in);
+        System.out.println("Does 3 x 3 = 9 ? // Type Y or N");
+       String str = kb.next();
+       System.out.println(str);
+       if ("Y".equals(str)) 
+            System.out.println("Good Job!\n What day in December is Joseph's"
+                    + " Birthday");
+         else
+           System.out.println("Not a valid choice!\n You're Dumb ...\n");
+       String str1 = kb.next();
+       if ("11".equals(str1))
+           System.out.println("you a real one");
+       else
+           System.out.println("it be like that");
+    }
+    
+}
